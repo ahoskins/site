5098492bbe3c60f8d3ee425a7c638fd0b08d2310
Andrew Hoskins <andrew.hoskins16@gmail.com>
2017-07-02 20:31:43 -0700

    remove atom import

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
 
-const atom = require('./lib/atom')
-
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
