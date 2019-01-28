7d95b03ab147a45e4921a3f2a90bafbbd953b197
Andrew Hoskins <arh.hoskins@gmail.com>
2019-01-27 19:46:59 -0800

    edits

--- pages/2019/incentives-for-a-better-world.js
+++ pages/2019/incentives-for-a-better-world.js
@@ -14,56 +14,58 @@ export default () => (
 
     <P>
     I've recently been struck with how significant "incentive design" is for shaping behavior of people and institutions. A choice made is some weighted function of many
-    inputs, sometimes chained on top of one another (like a Convolutional Neural Network). Single policies or environments will sway you into a particular direction.
+    inputs, sometimes layered on top of one another (reminds me sort of like a <a target="_blank" href="https://en.wikipedia.org/wiki/Convolutional_neural_network">Convolutional Neural Network</a>). These inputs can be ranging from government policies, weather, genetics, etc, and they all add up to sway you into a particular direction.
     </P>
 
     <P>
-    For example: in the choice of driving your car vs riding your bike, a policy that subsidizes parking will sway you towards driving. (And much to by dismay, making cities in
-    North America do this. They don't in <a target="_blank" href="https://medium.com/land-buildings-identity-and-values/tokyo-does-not-subsidise-its-transport-system-98f064f097b3">Tokyo!</a>). Free parking does not mean that everyone will choose driving every time over biking, but it does play it's role
-    to incentivize driving more.
+    For example: in the choice of driving your car vs riding your bike, a policy that subsidizes parking will sway you towards driving. (And much to by dismay, many cities in
+    North America do this by enforcing minimum parking requirements. They don't in <a target="_blank" href="https://medium.com/land-buildings-identity-and-values/tokyo-does-not-subsidise-its-transport-system-98f064f097b3">Tokyo!</a>). Free parking does not mean that everyone will choose driving every time over biking, but it does play its role
+    to incentivize more driving.
     </P>
 
     <P>
-    At an institutional level, it should be clear the affect of incentives because they are often directly measurable. A company may get a tax break to move money or invest
-    in office space in a particular location in the world (see the whole Amazon HQ2 drama). A less direct and measurable example is something like universities. What is
+    At an institutional/corporate level, the affect of incentives are sometimes clearly directly measurable. A company may get a tax break to move money or invest
+    in office space in a particular location in the world (see the whole Amazon HQ2 drama!). But sometimes the incentives are a bit less direct and measurable. For example: traditional universities. What is
     their incentive? They have no direct stake in their alumni future earnings. So their incentive is not to maximize future earnings of students. Whether one of their CS student becomes
-    the next billion dollar founder vs working in tech support for life has no direct relation to the universities earnings. Of course, many successful students will add up to
-    give their school pristige through association, and increase the chance of high alumni donations. But this is hard to measure, and has extremely lagging results spread way out over time (their alumni of today might not
-    be billionaires until they are 50 years old). So I would universities in general don't have much incentive for future earnings of students. That is, traditional universities.
+    the next billion dollar founder vs working in tech support for life has no direct relation to the universities earnings. It is true many successful students will add up to
+    give their school pristige through association, and increase the chance of high alumni donations (and I think this only really applies at the top ~1% of school because of unusual concentration of high-performers). But the impact of alumni success on university earnings is hard to measure because it has extremely lagging results spread way out over time (their alumni of today might not
+    be millionaires/billionaires until they are 50 years old). So I would say universities in general don't have a main incentive for future earnings of students. However, some non-traditional universities have flipped this!
     </P>
 
     <P>
     There are recent ventures like Lambda school who have recognized this perverse misalignment of incentive. Lambda school makes the incentive for future earnings of students very explicit in
-    it's business model. They charge no tuition up-front, and instead capture percent of future earnings for the next few years. To me, this is clearly better. This incentizes Lambda school
+    it's business model. They charge no tuition up-front, and instead capture percent of future earnings for the next few years. To me, this is clearly more optimal. This incentizes Lambda school
     to be actually true to their word of setting students up for successful careers. For more affirmation that Lamda school has got it right (which I strongly believe they do), follow <a target="_blank" href="https://twitter.com/AustenAllred">Austin on Twitter.</a>
-    He regularly reminds you of the deep sickness within traditional universities.    
+    He regularly reminds us of the deep sickness within traditional universities.    
     </P>
 
     <P>
-    I'm interested in exploring what else in the world has non-optimal incentive structures. Some lose thoughts:
+    I'm interested in exploring what else in the world has non-optimal incentive structures. Some loose thoughts about this topic in general (I'll likely append to this list over time):
     </P>
 
     <OL>
         <OLI>
-        Commission in retail jobs. This seems mostly correct because employees are motivated and directly rewarded for making a sale
+        Commission in retail jobs. This seems mostly correct because employees are motivated and directly rewarded for making a sale. It seems like this kind of incentive structure
+        could and maybe should be better implemented in other types of jobs (fast food, nursing, engineers). As something I'm familiar with, I think it's actually quite an inefficiency that software engineers don't
+        usually get bonuses / "commission" (there are many ways to measure tangible business impact) as a main form of compensation. I don't know if this would cause strange secondary effects, but I think doing something to motivate
+        engineers more towards "what's better for the company" rather than the often current bias towards "what's the more interesting problem to work on".
         </OLI>
 
         <OLI>
-        The free market of business in general has got it right. You will be rewarded through money for the value you capture through business. The local
-        restaurant deserves the millions they can make if they become wildy popular. Similarly, founders of Uber deserve the billions they make through their venture. It's like a very successful restaurant, but at a massive more complex scale.
-        involved in creating this business to be made billionaires.
+        Free market capitalism (in general) has got it right for incentive. You will be rewarded through money for the value you capture through business. The local
+        restaurant deserves the millions they can make if they become wildy popular. Similarly, founders of Uber deserve the billions they make through their business. I don't see any issue with scaling up the level of success from millions to billions.
         </OLI>
 
         <OLI>
         I feel there is some connection to incentives and good salespeople. Making a sale is usually about understanding what the buyer wants and what motivates them, 
-    rather than simply explaining the product you are selling. So at a root level, sales is about finding these incentive structures in a particular other group. 
+    rather than simply explaining the product you are selling. So at a root level, sales is about finding the buyer's incentive structures and exploiting them. 
         </OLI>
 
         <OLI>
          I want to revisit that example above with biking and driving. One of the things that frustrates me most on a daily basis in SF is cars on the streets that I'm biking on.
-         It's so absurd to me that they can drive aroud alone in those thousand pound massive pieces of metal on the streets without paying a fee everytime they use the road (especially main roads).
-         We should continue having more incentives that sway towards biking or walking, and away from driving one person in a car. More scooters the better, more competition in transportation
-         the better (let's go for some more privatized mass-transport like trains and other Uber-like inventions), more new models of bike ownership (like Vanmoof) the better.
+         It's absurd to me that they drive aroud alone in those thousand pound pieces of metal on the streets without paying a fee for using the roads (especially main roads shared with bikes and pedestians).
+         We should continue having more incentives that sway towards biking or walking, and away from driving your own car in cities. More scooters the better, more competition in transportation
+         the better (let's go for some more privatized mass-transport like trains and other Uber-like inventions), more new models of bike ownership (like <a target="_blank" href="https://www.vanmoof.com/en_us/plus">Vanmoof!</a>) the better.
         </OLI>
     </OL>
 
