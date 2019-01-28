ae1de42d100762902ccf453ad689ac7b4b6cd0bb
Andrew Hoskins <arh.hoskins@gmail.com>
2019-01-27 17:06:38 -0800

    modify home page a bit

--- pages/index.js
+++ pages/index.js
@@ -16,28 +16,28 @@ export default () => (
             <b>Summary:</b> I'm a software engineer in the day (sometimes at night). I'm interested in many ideas and experiences. I like <a className="no-margin" target="_blank" href="https://www.instagram.com/andrew.hoskins">exploring</a>, <Link href="/favorites"><a className="no-margin">learning</a></Link>, and <a className="no-margin" target="_blank" href="https://www.github.com/ahoskins">making things</a>. I live in San Francisco, but was raised in western Canada.
           </div>
           <div className="section">
-            <b>Work stuff:</b> I work at Affirm.
+            <b>Work stuff:</b> I work at Affirm. This website is not my Linkedin, so I'll direct to there for more details on professional experience (see link below).
           </div>
           <div className="section">
             <b>Other stuff:</b>
             <ul>
               <li>Fundamentally an optimist, even though it doesn't always appear that way</li>   
               <li>Wandering the streets of a new city is a great way to spend a day</li>           
-              <li>Interested in transportation and city infrastructure</li>
+              <li>City design is deeply important in affecting how people live daily</li>
               <li>Learning approach is, "throw me into the deep end, and I'll improve over time"</li>              
               <li>Love travel, but bad at learning new language and remembering names or details</li>          
-              <li>If feeling tired or unmotivated, get outside in the sun</li>  
-              <li>Aim to do some kind of dedicated physical exercise once a day</li>      
+              <li>If feeling tired / unmotivated, getting outside in the sun always helps</li>      
               <li>Cheap street food (where cooking can be seen and smelled) is often the best food</li>  
               <li>A small portion of people do most of the original thinking for society, others follow</li>       
-              <li>People should be more aware of cognitive biases</li>             
+              <li>People should be more aware of cognitive biases</li> 
+              <li>We should be more keenly aware of incentive structures</li>             
               <li>Gratitude is underrated, even though it's cliche</li>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
             </ul>
           </div>
         </div>
         <br />
         <nav>
-          <a target="_blank" href="https://twitter.com/arhhoskins">Twitter</a>
+          <a target="_blank" href="https://twitter.com/_ahoskins">Twitter</a>
           <Link href="/writing"><a>Writing</a></Link>
           <a target="_blank" href="https://www.linkedin.com/in/andrew-hoskins-05825991/">LinkedIn</a>
         </nav>
